5731533e4f82f027a267d6b8316a950dbaec1133
Tim Kral <mail@timkral.com>
2024-10-04 11:29:52 +0200

    implement drag-and-drop for pieces

---
+++ frontend/src/js/board.js
@@ -0,0 +1,40 @@
+((func) => {
+  func();
+})(() => {
+  /**
+   * @type {HTMLLIElement[]}
+   */
+  const boardTiles = document.querySelectorAll(".chess-board>li");
+  /**
+   * @type {HTMLImageElement[]}
+   */
+  const pieces = document.querySelectorAll(".chess-board>li>img");
+
+  function handleDragStart() {
+    this.ctx.piece = this.piece;
+  }
+
+  function handleDragEnter() {
+    this.ctx.tile = this.tile;
+  }
+
+  function handleDragEnd() {
+    const { tile, piece } = this.ctx;
+
+    piece.remove();
+    tile.appendChild(piece);
+  }
+
+  const context = {};
+
+  boardTiles.forEach((tile) => {
+    tile.addEventListener("dragover", ev => ev.preventDefault());
+    tile.addEventListener("dragenter", handleDragEnter.bind({ ctx: context, tile }));
+    tile.addEventListener("dragend", handleDragEnd.bind({ ctx: context, tile }));
+    //tile.addEventListener("drop", console.log);
+  });
+
+  pieces.forEach((piece) => {
+    piece.addEventListener('dragstart', handleDragStart.bind({ ctx: context, piece }));
+  });
+});
