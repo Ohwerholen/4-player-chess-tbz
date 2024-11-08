c66e0af49c6b8be2d1bb266d1c1c2f925e361b46
Tim Kral <mail@timkral.com>
2024-11-08 12:53:51 +0100

    implement validation library in frontend

--- frontend/src/js/board.js
+++ frontend/src/js/board.js
@@ -1,44 +1,62 @@
-((func) => {
-  func();
-})(() => {
-  /**
-   * @type {HTMLLIElement[]}
-   */
-  const boardTiles = document.querySelectorAll(".chess-board>li");
-  /**
-   * @type {HTMLImageElement[]}
-   */
-  const pieces = document.querySelectorAll(".chess-board>li>img");
+import { Board, BoardConfiguration } from "/validation.js";
 
-  function handleDragStart() {
-    this.ctx.piece = this.piece;
+/**
+ * @type {HTMLLIElement[]}
+ */
+const boardTiles = document.querySelectorAll(".chess-board>li");
+/**
+ * @type {HTMLImageElement[]}
+ */
+const pieces = document.querySelectorAll(".chess-board>li>img");
+
+const board = new Board(BoardConfiguration.getDefaultConfiguration(undefined));
+
+let ny = 0;
+for (let y = 11; y >= 0; y--) {
+  for (let x = 0; x < 12; x++) {
+    let tile = boardTiles[y * 13 + x];
+    tile.setAttribute("data-index", (ny * 12 + x).toString());
   }
+  ny++;
+}
 
-  function handleDragEnter() {
-    this.ctx.tile = this.tile;
-  }
+function handleDragStart() {
+  this.ctx.piece = this.piece;
+}
 
-  function handleDragEnd() {
-    const { tile, piece } = this.ctx;
-    const priorPiece = tile.querySelector('*');
+function handleDragEnter() {
+  this.ctx.tile = this.tile;
+}
 
-    if (priorPiece !== null)
-      priorPiece.remove();
+function handleDragEnd() {
+  const { tile, piece } = this.ctx;
+  const priorPiece = tile.querySelector("*");
 
+  const currentIndex = parseInt(piece.parentNode.getAttribute("data-index"));
+  const targetIndex = parseInt(tile.getAttribute("data-index"));
+
+  if (board.move(currentIndex, targetIndex)) {
+  if (priorPiece !== null) priorPiece.remove();
     piece.remove();
     tile.appendChild(piece);
   }
+}
 
-  const context = {};
+const context = {};
 
-  boardTiles.forEach((tile) => {
-    tile.addEventListener("dragover", ev => ev.preventDefault());
-    tile.addEventListener("dragenter", handleDragEnter.bind({ ctx: context, tile }));
-    tile.addEventListener("dragend", handleDragEnd.bind({ ctx: context, tile }));
-    //tile.addEventListener("drop", console.log);
-  });
+boardTiles.forEach((tile) => {
+  tile.addEventListener("dragover", (ev) => ev.preventDefault());
+  tile.addEventListener(
+    "dragenter",
+    handleDragEnter.bind({ ctx: context, tile }),
+  );
+  tile.addEventListener("dragend", handleDragEnd.bind({ ctx: context, tile }));
+  //tile.addEventListener("drop", console.log);
+});
 
-  pieces.forEach((piece) => {
-    piece.addEventListener('dragstart', handleDragStart.bind({ ctx: context, piece }));
-  });
+pieces.forEach((piece) => {
+  piece.addEventListener(
+    "dragstart",
+    handleDragStart.bind({ ctx: context, piece }),
+  );
 });
--- libraries/test.ts
+++ libraries/test.ts
@@ -1,13 +1,13 @@
-import { Board, BoardConfiguration } from "./validation/index";
+import { Board, BoardConfiguration, BoardUtilities } from "./validation/index";
 
 const board = new Board(BoardConfiguration.getDefaultConfiguration(undefined));
 
-console.log(board.mapPlayers());
+console.log(board.mapPieces());
 console.log('------------');
 
-//console.log(board.move('B3', 'B5'));
-console.log(board.move('B1', 'B6'));
+console.log(board.move(2, 'C5'));
+:w//console.log(board.move('B1', 'F6'));
 console.log('------------');
 
-console.log(board.mapPlayers());
+console.log(board.mapPieces());
 console.log('------------');
